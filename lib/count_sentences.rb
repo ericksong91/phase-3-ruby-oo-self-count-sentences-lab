e92743c3b891f675165ec8c5f924133dc351b42c
Eric Song <ericksong91@gmail.com>
2022-12-20 15:05:26 -0500

    Done with assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
 
+    self.end_with?("?")
+
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
 
+    pp self
+
+    new_array = self.split(/\.|\?|\!/)
+    pp new_array.filter {|sent| !sent.empty?}.length
+
   end
 end
+
+"Hello world!!!!! My Name is Harold. I wouldn't like to be your friend. Unless?".count_sentences
